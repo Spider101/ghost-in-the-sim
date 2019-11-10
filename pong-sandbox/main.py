43d88aa3d89f9ee67e874fc17652bd7c41627575
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2019-11-10 23:00:35 +0530

    pong-sandbox-2: add Pipfile, add logic for hitbox, wall boundaries and entity movement

--- pong-sandbox/main.py
+++ pong-sandbox/main.py
@@ -1,16 +1,45 @@
 import pygame
 
-from pong import Ball, Player
+from pong import Ball, Player, WIDTH, HEIGHT
 
-def start_game():
-    pass
+def start_game(window):
+    player1 = Player(True, 'Player 1')
+    player2 = Player(False, 'Player2')
+    ball = Ball(1, True)
+
+    shouldRun = True
+    while shouldRun:
+        pygame.time.delay(200)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                shouldRun = False
+
+        ball.update_coords([player1.paddle, player2.paddle])
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            player1.paddle.update_coords(1)
+        elif keys[pygame.K_DOWN]:
+            player1.paddle.update_coords(-1)
+        elif keys[pygame.K_LEFT]:
+            player2.paddle.update_coords(1)
+        elif keys[pygame.K_RIGHT]:
+            player2.paddle.update_coords(-1)
+        
+        window.fill((0,0,0))
+        
+        player1.paddle.draw(window)
+        player2.paddle.draw(window)
+        ball.draw(window)
+
+        pygame.display.update()
 
 if __name__ == '__main__':
     pygame.init()
-    
-    player = Player()
-    ball = Ball()
-    
-    start_game()
+   
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('I am nice at ping pong')
+    start_game(win)
 
-
+    pygame.quit()
--- pong-sandbox/pong.py
+++ pong-sandbox/pong.py
@@ -1,26 +1,52 @@
+import pygame
 WIDTH, HEIGHT = 500, 500 
 
 class Ball:
-    def __init__(self, difficulty):
-        self.coords = [WIDTH / 2, HEIGHT / 2]
-        self._radius = 50
-        self._vel = 2
+    def __init__(self, difficulty, willMoveLeft):
+        self.coords = [ WIDTH // 2, HEIGHT // 2 ]
+        self._radius = 10 
+        self._init_vel = [-2 if willMoveLeft else 2, -2]
         self._speed_boost = difficulty
+        self.velocity = self.initial_velocity
 
     @property
     def radius(self):
         return self._radius
 
     @property
-    def velocity(self):
-        return self._vel
+    def initial_velocity(self):
+        return self._init_vel
 
     @property
     def speed_boost(self):
         return self._speed_boost
 
-    def update_coords(self):
-        pass
+    def reset(self):
+        self.coords = [ WIDTH // 2, HEIGHT // 2 ]
+        self.velocity = self.initial_velocity
+
+    def update_coords(self, paddles):
+        paddle_gutters = sorted([paddles[0].gutter_boundary, paddles[1].gutter_boundary])
+        paddle_offsets = [-1 * paddles[0].height // 2, paddles[0].height // 2]
+        paddle_hitboxes = [ [ paddle.y_pos + offset for offset in paddle_offsets ] for paddle in paddles ]
+
+        x, y = [ coord + vel for coord, vel in zip(self.coords, self.velocity) ]
+
+        if x - self.radius <= paddle_gutters[0] or x + self.radius >= paddle_gutters[1]:
+            if (paddle_hitboxes[0][0] <= y <= paddle_hitboxes[0][1]
+                or paddle_hitboxes[1][0] <= y <= paddle_hitboxes[1][1]):
+                self.velocity = [ vel * self.speed_boost for vel in self.velocity ]
+                self.velocity[0] *= -1
+            else:
+                self.reset()
+
+        elif y + self.radius >= HEIGHT or y - self.radius <= 0:
+            self.velocity[1]  *= -1
+
+        self.coords = [ coord + vel for coord, vel in zip(self.coords, self.velocity)]
+
+    def draw(self, window):
+        pygame.draw.circle(window, (255, 255, 255), self.coords, self.radius)
 
 class Player:
     def __init__(self, isTurn, alias):
@@ -31,10 +57,12 @@ class Player:
 
 class Paddle:
     def __init__(self, isLeft):
-        self._vel = 2
-        self._height = 20
-        self._width = 5
-        self.coords = (0, HEIGHT / 2 - self._height / 2) if isLeft else (WIDTH - self._width, HEIGHT / 2 - self._height / 2)
+        self._vel = -4
+        self._height = 40
+        self._width = 10
+        self._gutter_boundary = self.width if isLeft else WIDTH - self.width
+        self.y_pos = (HEIGHT - self.height ) // 2
+        self._x_pos = 0 if isLeft else WIDTH - self.width
 
     @property
     def height(self):
@@ -45,8 +73,26 @@ class Paddle:
         return self._width
 
     @property
+    def x_pos(self):
+        return self._x_pos
+
+    @property
     def velocity(self):
         return self._vel
 
-    def update_coords(self):
-        pass
+    @property
+    def gutter_boundary(self):
+        return self._gutter_boundary
+
+    def update_coords(self, sign):
+        y = self.y_pos + sign * self.velocity
+        wall_gutters = [ 0, HEIGHT - self.height ]
+        if wall_gutters[0] < y < wall_gutters[1]:
+            self.y_pos = y
+        elif y < wall_gutters[0]:
+            self.y_pos = wall_gutters[0]
+        elif y > wall_gutters[1]:
+            self.y_pos = wall_gutters[1] 
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 255, 255), (self.x_pos, self.y_pos, self.width, self.height))
